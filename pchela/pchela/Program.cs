455d1e262baca53d81b96dad1bac5116e2517795
Enneigardik <57490588+Enneigardik@users.noreply.github.com>
2021-02-26 15:58:07 +0300

    Update Program.cs

--- pchela/pchela/Program.cs
+++ pchela/pchela/Program.cs
@@ -1,35 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace pchela
+namespace vectora
 {
-     class MyStack <T>
+    class MyVector<T>
     {
         private T[] data;
-        private int size;
-
-        MyStack()
+        public int size;
+        public T this[int key]
         {
-            size = 0;
-            data = new T[0];
-        }
-        public void Push (T x)
-        {
-            Array.Resize(ref data, ++size);
-            data[size-1]=x;
-        }
-
-        public void Pop()
-        {
-            if(size!=0)
+            get
             {
-                Array.Resize(ref data, --size);
+                return data[key];
+            }
+            set
+            {
+                data[key] = value;
             }
         }
-        
-        public T Peek()
+
+        public int Count
         {
-            if (size!=0)
+            get
+            {
+                return size;
+            }
+        }
+
+        public void Add(T x)
+        {
+            Array.Resize(ref data, ++size);
+            data[size - 1] = x;
+        }
+        public T Insert(T x, int numer)
+        {
+           
+        }
+        public T RemoveAt(int numer)
+        {
+            if (size != 0)
+            {
+
+                var t = data[numer];
+                Array.Resize(ref data, --size);
+                return t;
+            }
+            return default;
+        }
+
+        public T Last()
+        {
+            if (size != 0)
             {
                 return data[size - 1];
             }
@@ -38,129 +59,39 @@ namespace pchela
                 return default(T);
             }
         }
-        public bool isEmpty()
+        public T First()
         {
-            return size == 0;
+            if (size != 0)
+            {
+                return data[0];
+            }
+            else
+            {
+                return default(T);
+            }
         }
+
         public void Clear()
         {
             size = 0;
             data = new T[0];
         }
-    }   
+
+
+
+    }
 
     class Program
     {
-        static int[,] max;
-        static MyStack<KeyValuePair<int, int>> stack = new MyStack<KeyValuePair<int, int>>();
+
+        public static MyVector<List<int>> vect = new MyVector<List<int>>();
         static void Main(string[] args)
         {
-            int p = 1000;
-            max = new int[p, p];
-            Console.WriteLine("Изначальное");
-            Create(p);
-            
-            Schet(max);
-            Console.WriteLine("Заливка");
-            //fill(2, 2, max);
-            
-            Schet(max);
-            Console.WriteLine("Непонятные стеки");
-            stackk(2, 2, max);
-            
-            Schet(max);
-        }
-        public static void Create(int p)
-        {
-            max = new int[p, p];
-            Random hate = new Random(2);
-            for (int i = 0; i < p; i++)
-            {
-                for (int j = 0; j < p; j++)
-                {
-                    if (hate.Next(0, 4) == 1)
-                    {
-                        max[i, j] = 1;
-                    }
-                }
-            }
+            List<int> AAA = new List<int>();
+
 
         }
 
-        public static void Show(int[,] max)
-        {
-            for (int i = 0; i < max.GetLength(0); i++)
-            {
-                for (int j = 0; j < max.GetLength(0); j++)
-                {
-                    Console.Write(Convert.ToString(max[i, j]) + " ");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-        }
-        public static void fill(int x, int y, int[,] max)
-        {
-            const int c = 2;
-            if (x >= 0 && y >= 0 && x < max.GetLength(0) && y < max.GetLength(0) && max[x, y] == 0)
-            {
-                max[x, y] = c;
-                fill(x + 1, y, max);
-                fill(x - 1, y, max);
-                fill(x, y + 1, max);
-                fill(x, y - 1, max);
-            }
-
-        }
-
-        public static void stackk(int x, int y, int[,] max)
-        {
-            const int c = 2;
-            stack.Push(new KeyValuePair<int, int> ( x, y ));
-            while (stack.Count > 0)
-            {
-                var st = stack.Pop();
-                if (st.Key >= 0 && st.Value >= 0 && st.Key < max.GetLength(0) &&
-                    st.Value < max.GetLength(0) &&
-                    max[st.Key, st.Value] == 0)
-                { 
-                    max[st.Key, st.Value] = c;
-                    stack.Push(new KeyValuePair<int, int>( st.Key, st.Value - 1 ));
-                    stack.Push(new KeyValuePair<int, int>( st.Key - 1, st.Value ));
-                    stack.Push(new KeyValuePair<int, int> ( st.Key, st.Value + 1 ));
-                    stack.Push(new KeyValuePair<int, int>( st.Key + 1, st.Value ));
-                }
-            }
-        }
-
-        static void Schet(int[,] matrix)
-        {
-            int a1 = 0;
-            int b2 = 0;
-            int c0 = 0;
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(0); j++)
-                {
-                    if (matrix[i, j] == 1)
-                    {
-                        a1++;
-                    }
-                    if (matrix[i, j] == 2)
-                    {
-                        b2++;
-                    }
-                    if (matrix[i, j] == 0)
-                    {
-                        c0++;
-                    }
-                }
-            }
-
-            Console.WriteLine("1 - " + a1);
-            Console.WriteLine("2 - " + b2);
-            Console.WriteLine("0 - " + c0);
-            Console.WriteLine();
-        }
     }
 }
+
