82b0a537d3f00b85d053b882445c08a4eeada44b
Enneigardik <57490588+Enneigardik@users.noreply.github.com>
2021-03-05 15:58:35 +0300

    Update Program.cs

--- pchela/pchela/Program.cs
+++ pchela/pchela/Program.cs
@@ -1,8 +1,25 @@
 using System;
 using System.Collections.Generic;
-
-namespace vectora
+namespace Galko
 {
+    public class Keyval
+    {
+        public int x;
+        public int y;
+        public Keyval(int a, int b)
+        {
+            x = a;
+            y = b;
+        }
+    }
+    public class MyComp:IComparer<Keyval>
+    {
+        public int Compare(Keyval l, Keyval r)
+        {
+            return l.y < r.y ? -1 : 1;
+        }
+    }
+
     class MyVector<T>
     {
         int size = 0;
@@ -70,8 +87,8 @@ namespace vectora
             }
             return default;
         }
-        
-        public T Lastl()
+
+        public T Last()
         {
             if (size != 0)
             {
@@ -100,25 +117,67 @@ namespace vectora
             size = 0;
             data = new T[0];
         }
+        bool Contains(T item)
+        {
 
-
-
+        }
     }
+
 
     class Program
     {
-
+        delegate int g(int a, int b);
+        delegate int f(int x);
         public static MyVector<int> hate = new MyVector<int>();
         static void Main(string[] args)
         {
-            hate.Add(2);
-            Console.Write(hate[0]);
-            hate.Insert(5, 5);
-            hate.Insert(-5, -5);
-            Console.WriteLine(hate[5]);
-           
-        }
+            //f sq = x => x * x;
+            //Console.WriteLine(sq(5));
+            //g sum = (x, y) => x + y;
+            //Console.WriteLine(sum(4, 5));
 
+            //System.Action<int> aa = null;
+            //aa = x => Console.WriteLine(x);
+            //aa(5);
+
+            //System.Func<int, int, int> ff = null;
+            //ff = (a, b) =>
+            //  {
+            //      if (a < b) return a + b;
+            //      else return a * a + b * b;
+
+            //  };
+            //Console.WriteLine(ff(4, 5));
+
+            //Func<int, int> fip = null;
+            //fip = x => x > 1 ? fip(x - 1) + fip(x - 2) : x;
+            //Console.WriteLine(fip(10));
+
+
+
+            //var m = new Keyval[5];
+            //m[0] = new Keyval(2, 5);
+            //m[1] = new Keyval(4, 4);
+            //m[2] = new Keyval(6, 3);
+            //m[3] = new Keyval(8, 2);
+            //m[4] = new Keyval(10, 1);
+            ////Array.Sort(m, new MyComp());
+
+            //Array.Sort(m, (a, b) => a.y.CompareTo(b.y));
+            //foreach (var l in m)
+            //{
+            //    Console.WriteLine(l.x + " " + l.y);
+            //}
+
+            //var lst = new List<int> { 0, 1, 2, 3, 4 };
+            //Console.WriteLine(lst.FindIndex(x => x == 3));
+            //lst.ForEach(x => Console.WriteLine(x));
+
+            //hate.Add(2);
+            //Console.Write(hate[0]);
+            //hate.Insert(5, 5);
+            //hate.Insert(-5, -5);
+            //Console.WriteLine(hate[5]);
+        }
     }
 }
-
