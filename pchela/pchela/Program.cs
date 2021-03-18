a675589e673952a5b08df046299ad86d999ff825
Enneigardik <57490588+Enneigardik@users.noreply.github.com>
2021-03-18 19:01:15 +0300

    Update Program.cs

--- pchela/pchela/Program.cs
+++ pchela/pchela/Program.cs
@@ -1,203 +1,139 @@
 using System;
 using System.Collections.Generic;
-namespace Galko
+
+namespace SAOD4
 {
-    public class Keyval
-    {
-        public int x;
-        public int y;
-        public Keyval(int a, int b)
-        {
-            x = a;
-            y = b;
-        }
-    }
-    public class MyComp : IComparer<Keyval>
-    {
-        public int Compare(Keyval l, Keyval r)
-        {
-            return l.y < r.y ? -1 : 1;
-        }
-    }
+	public class ListNode<T> 
+	{
+		public T Value;
+		public ListNode<T> Next { get; set; }
+		public ListNode(T value)
+		{
+			Value = value;
+		}
+	}
+	public class YourLinkedList<T> 
+	{
+		public int Count { private set; get; }
+		private ListNode<T> first;
+		public T First => Count > 0 ? first.Value : default;
+		public T Last => Count > 0 ? this[Count - 1] : default;
 
-    class MyVector<T>
-    {
-        public int Count => size;
-        public bool IsEmpty => size == 0;
+		public T this[int i]
+		{
+			get
+			{
+				if (i < 0 || i >= Count)
+					throw new IndexOutOfRangeException("Индекс вышел за границы списка: i = " + i + "; Count = " + Count);
+				return GetNode(i).Value;
+			}
+			set
+			{
+				if (i < 0 || i >= Count)
+					throw new IndexOutOfRangeException("Индекс вышел за границы списка: i = " + i + "; Count = " + Count);
+				GetNode(i).Value = value;
+			}
+		}
 
-        private int size = 0;
-        private T[] data = new T[0];
-        
-        public T this[int n]
-        {
-            get
-            {
-                if (n < 0 || n > size)
-                    throw new IndexOutOfRangeException("Я ЗАПРЕЩАЮ ВАМ ТУТ ТРОГАТЬ");
-                return data[n];
-            }
-            set
-            {
+		public void AddFirst(T element)
+		{
+			var node = new ListNode<T>(element);
+			if (Count > 0)
+				node.Next = first;
+			first = node;
+			Count++;
+		}
+		public void AddLast(T element)
+		{
+			var node = new ListNode<T>(element);
+			if (Count > 0)
+				GetNode(Count - 1).Next = node;
+			else
+				first = node;
+			Count++;
+		}
+		public void Insert(int index, T element)
+		{
+			if (index == Count) AddLast(element);
+			else if (index == 0) AddFirst(element);
+			else
+			{
+				var next = GetNode(index);
+				var prev = GetNode(index - 1);
+				var node = new ListNode<T>(element);
+				node.Next = next;
+				prev.Next = node;
+				Count++;
+			}
+		}
+		public void RemoveAt(int index)
+		{
+			var node = GetNode(index);
+			if (Count == 1)
+				first = null;
+			else if (first == node)
+				first = node.Next;
+			else
+				GetNode(index - 1).Next = node.Next;
+			Count--;
+		}
+		public int IndexOf(T element)
+		{
+			var cur = first;
+			if (object.Equals(cur.Value, element)) return 0;
+			for (var i = 1; i < Count; i++)
+			{
+				cur = cur.Next;
+				if (object.Equals(cur.Value, element)) return i;
+			}
+			return -1;
+		}
+		public void Clear()
+		{
+			first = null;
+			Count = 0;
+		}
 
-                if (n < 0 || n > size)
-                    throw new IndexOutOfRangeException("Я ЗАПРЕЩАЮ ВАМ ТУТ ТРОГАТЬ");
-                data[n] = value;
-            }
-        }
+		public T[] ToArray()
+		{
+			var array = new T[Count];
+			if (Count == 0) return array;
+			var el = first;
+			array[0] = el.Value;
+			for (var i = 1; i < Count; i++)
+			{
+				el = el.Next;
+				array[i] = el.Value;
+			}
+			return array;
+		}
+		public bool Contains(T element) =>
+			IndexOf(element) != -1;
+		public IEnumerator<T> GetEnumerator() =>
+			ToArray().GetEnumerator() as IEnumerator<T>;
 
+		private ListNode<T> GetNode(int index)
+		{
+			var cur = first;
+			for (var j = 0; j < index; j++) cur = cur.Next;
+			return cur;
+		}
+	}
+	class Programm
+	{
+		static void Main(string[] args)
+		{
+			var list = new YourLinkedList<int>();
+			list.AddLast(9);
+			list.AddLast(1);
+			list.AddLast(2);
+			list.AddLast(3);
+			for (int i = 0; i < list.Count; i++)
+			{
+				Console.Write(list[i] + "=>");
+			}
+			Console.WriteLine("null");
+		}
 
-        public void Add(T x)
-        {
-            Resize(++size);
-            data[size - 1] = x;
-        }
-
-        public void Insert(int x, T numer)
-        {
-            if (x < 0)
-                throw new IndexOutOfRangeException("А как");
-            if (x > size)
-            {
-                Resize(x + 1);
-                data[x] = numer;
-            }
-            else
-            {
-                size++;
-                Resize(size);
-                for (int i = size - 1; i > x; i--)
-                    data[i] = data[i - 1];
-                data[x] = numer;
-            }
-        }
-
-        public T RemoveAt(int numer)
-        {
-            if (size != 0)
-            {
-                var t = data[numer];
-                for (int i = numer+1; i < size; i++)
-                    data[i - 1] = data[i];
-                Resize(--size);
-                return t;
-            }
-            return default;
-        }
-
-        public T Last() =>
-            size != 0 ? data[size - 1] : default;
-
-        public T First() =>
-            size != 0 ? data[0] : default;
-
-        public void Clear()
-        {
-            size = 0;
-            data = new T[0];
-        }
-        public bool Contains(T item) =>
-            IndexOf(item) != -1;
-        public int IndexOf(T item)
-        {
-            for (int i=0; i<size; i++)
-                if (object.Equals(item, data[i]))
-                    return i;
-            return -1;
-        }
-        public IEnumerator<T> GetEnumerator() =>
-            data.GetEnumerator() as IEnumerator<T>;
-
-        public void ForEach(Action<T> action)
-        {
-            for(int i = 0; i < size; i++)
-                action(data[i]);
-        }
-        public T Find(Predicate<T> match)
-        {
-            for (int i = 0; i < size; i++)
-                if (match(data[i]))
-                    return data[i];
-            return default;
-        }
-        public int FindIndex(Predicate<T> match)
-        {
-            for (int i = 0; i < size; i++)
-                if (match(data[i]))
-                    return i;
-            return -1;
-        }
-        private void Resize(int newSize)
-        {
-            newSize = (int) Math.Pow(2, Math.Ceiling(Math.Log2(newSize)));
-            if (newSize != data.Length)
-                Array.Resize(ref data, newSize);  
-        }
-    }
-
-
-    class Program
-    {
-        delegate int g(int a, int b);
-        delegate int f(int x);
-        public static MyVector<int> hate = new MyVector<int>();
-        static void Main(string[] args)
-        {
-            hate.Add(1);
-            hate.Insert(0, 2);
-            hate.Add(4);
-            hate.ForEach(i => Console.Write(i + " "));
-            hate.Add(5);
-            hate.Add(8);
-            Console.WriteLine();
-            hate.ForEach(i => Console.Write(i + " "));
-            Console.WriteLine(hate.Contains(1));
-            //f sq = x => x * x;
-            //Console.WriteLine(sq(5));
-            //g sum = (x, y) => x + y;
-            //Console.WriteLine(sum(4, 5));
-
-            //System.Action<int> aa = null;
-            //aa = x => Console.WriteLine(x);
-            //aa(5);
-
-            //System.Func<int, int, int> ff = null;
-            //ff = (a, b) =>
-            //  {
-            //      if (a < b) return a + b;
-            //      else return a * a + b * b;
-
-            //  };
-            //Console.WriteLine(ff(4, 5));
-
-            //Func<int, int> fip = null;
-            //fip = x => x > 1 ? fip(x - 1) + fip(x - 2) : x;
-            //Console.WriteLine(fip(10));
-
-
-
-            //var m = new Keyval[5];
-            //m[0] = new Keyval(2, 5);
-            //m[1] = new Keyval(4, 4);
-            //m[2] = new Keyval(6, 3);
-            //m[3] = new Keyval(8, 2);
-            //m[4] = new Keyval(10, 1);
-            ////Array.Sort(m, new MyComp());
-
-            //Array.Sort(m, (a, b) => a.y.CompareTo(b.y));
-            //foreach (var l in m)
-            //{
-            //    Console.WriteLine(l.x + " " + l.y);
-            //}
-
-            //var lst = new List<int> { 0, 1, 2, 3, 4 };
-            //Console.WriteLine(lst.FindIndex(x => x == 3));
-            //lst.ForEach(x => Console.WriteLine(x));
-
-            //hate.Add(2);
-            //Console.Write(hate[0]);
-            //hate.Insert(5, 5);
-            //hate.Insert(-5, -5);
-            //Console.WriteLine(hate[5]);
-        }
-    }
+	}
 }
